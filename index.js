d14942a4d7e91bc125446a8439b3fb79a1a7e3a2
chriswait <2563474+chriswait@users.noreply.github.com>
2022-01-31 10:31:39 +0000

    post to slack when beginning audit

--- index.js
+++ index.js
@@ -249,6 +249,7 @@ const requestHandler = (request, response) => {
 
         // Then start running audits and generating reports
         const REPORT_DIR = process.env.REPORTDIR || '';
+        postToSlack('Starting audit');
         const reports = await runScriptWithBatch(script, batch, server);
         await Promise.all(reports.map(async report => {
           // Generate html report
